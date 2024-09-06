9f809282bdd2e5e3d8456cf1e188cebeb90b13b4
Pranjali Bajpai <115078315+Pranjalibajpai123@users.noreply.github.com>
2024-09-06 05:02:06 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,36 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)  //Datatype is written as Number with N capital
+// console.log(typeof valueInNumber);
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanIsLoggedIn );
+
+// let isLoggedIn = ""
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanIsLoggedIn );
+
+let isLoggedIn = "Pranjali"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn );
+
+// 1 => true; 0 => false
+// "" => false
+// "pranjali" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
