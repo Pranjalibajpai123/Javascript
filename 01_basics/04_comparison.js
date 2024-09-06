12a54a404a6356ebc61672bb55a88b580523880c
Pranjali Bajpai <115078315+Pranjalibajpai123@users.noreply.github.com>
2024-09-06 11:06:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+/* console.log("2"> 1);
+console.log("02" > 1); */
+
+/*console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  // it converts null to 0
+
+console.log(undefined == 0);
+
+console.log(undefined > 0);
+
+console.log(undefined < 0); // In js comparison and equality operations are considered different */
+
+// strict check(===) it strictly checks value, not just a value, but datatype as well
+console.log("2" === 2);
+
+
