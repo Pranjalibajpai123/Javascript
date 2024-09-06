9696b62741831d8c2f77277156c9594c62392278
Pranjali Bajpai <115078315+Pranjalibajpai123@users.noreply.github.com>
2024-09-06 10:20:30 +0000

    conversion-operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -19,7 +19,7 @@ let valueInNumber = Number(score)  //Datatype is written as Number with N capita
 let isLoggedIn = "Pranjali"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log( booleanIsLoggedIn );
+//console.log( booleanIsLoggedIn );
 
 // 1 => true; 0 => false
 // "" => false
@@ -28,8 +28,42 @@ console.log( booleanIsLoggedIn );
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************Operations*******************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2); */
+
+/*let str1 = "pranjali"
+let str2 = " Bajpai"
+
+let str3 = str1 + str2
+console.log(str3); */
+
+
+/*console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); */
+
+/*let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); */
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
 
 
 
